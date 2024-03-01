6eb82a6a23af0a30a67574c92472eccdf3db6c65
Dionna <businessladythick@yahoo.com>
2024-03-01 17:17:25 -0500

    started with some small basics

---
+++ fundamentals.js
@@ -0,0 +1,9 @@
+//This is the provided Course Info
+const courseInfo = {
+    id: 1234,
+    name: "Introduction to JavaScript Part 1"
+
+};
+console.log(courseInfo.id);
+console.log(courseInfo.name);
+//just started with a few small basics
